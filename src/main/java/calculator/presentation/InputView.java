d9fd6c47e64601ccaa183fbcb715c15ad9846ee1
rhwlgns4386 <rhwlgns4386@naver.com>
2025-10-15 03:38:04 +0900

    refactor: 매직넘버 상수화

--- src/main/java/calculator/presentation/InputView.java
+++ src/main/java/calculator/presentation/InputView.java
@@ -4,11 +4,13 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
+    private static final String PRINT_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+
     private InputView() {
     }
 
     public static String readExpression() {
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        System.out.println(PRINT_MESSAGE);
         return Console.readLine();
     }
 }
--- src/main/java/calculator/presentation/OutputView.java
+++ src/main/java/calculator/presentation/OutputView.java
@@ -2,10 +2,12 @@ package calculator.presentation;
 
 public class OutputView {
 
+    public static final String OUTPUT_MESSAGE_PREFIX = "결과 : ";
+
     private OutputView() {
     }
 
     public static void printResult(String result) {
-        System.out.printf("결과 : %s\n", result);
+        System.out.println(OUTPUT_MESSAGE_PREFIX + result);
     }
 }

2c459167f7e3c344a7820a27123e56037215b789
rhwlgns4386 <rhwlgns4386@naver.com>
2025-10-15 03:27:29 +0900

    refactor: 누락된 private 기본생성자 추가

--- src/main/java/calculator/presentation/InputView.java
+++ src/main/java/calculator/presentation/InputView.java
@@ -4,6 +4,9 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
+    private InputView() {
+    }
+
     public static String readExpression() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         return Console.readLine();
--- src/main/java/calculator/presentation/OutputView.java
+++ src/main/java/calculator/presentation/OutputView.java
@@ -2,6 +2,9 @@ package calculator.presentation;
 
 public class OutputView {
 
+    private OutputView() {
+    }
+
     public static void printResult(String result) {
         System.out.printf("결과 : %s\n", result);
     }
